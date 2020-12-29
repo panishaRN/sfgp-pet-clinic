db23fc4af27e3f47e56b0c359178e79b14e33178
pnagendra <panisha.nagendra@solenix.ch>
2020-12-29 13:54:16 +0100

    INITIAL COMMIT

---
+++ src/main/java/guru/springframework/sfgppetclinic/SfgpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgppetclinic/SfgpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
