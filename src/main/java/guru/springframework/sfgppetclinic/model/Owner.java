7abfae9f8e6fce8c579e3e6cc364801d7b6aaff1
pnagendra <panisha.nagendra@solenix.ch>
2020-12-30 23:03:23 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgppetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgppetclinic.model;
+
+public class Owner extends  Person{
+}
---
+++ src/main/java/guru/springframework/sfgppetclinic/model/Vat.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgppetclinic.model;
+
+public class Vat extends  Person{
+}
